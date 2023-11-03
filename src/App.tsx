4e3f3ac714d60a0383ed8a5b9ad641fc1f253e3a
Amrutha J Raj <A-9751@ibsplc.com>
2023-11-03 12:44:42 +0530

    Changes initial design

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import { Store } from "./pages/Store";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container></>
   );
 }
 
